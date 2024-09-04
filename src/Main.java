8ac0a8e1b51238637b3a09212fa75ec307baf189
Lorgus <facelsskyubei@gmail.com>
2024-09-04 15:49:46 +0300

    .

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Date;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+            Rectangle rect1 = new Rectangle(4, 40);
+            Rectangle rect2 = new Rectangle(3.5, 35.9);
+
+
+            System.out.println("Rectangle 1:");
+            System.out.println("Width: " + rect1.getWidth());
+            System.out.println("Height: " + rect1.getHeight());
+            System.out.println("Area: " + rect1.getArea());
+            System.out.println("Perimeter: " + rect1.getPerimeter());
+
+
+            System.out.println("\nRectangle 2:");
+            System.out.println("Width: " + rect2.getWidth());
+            System.out.println("Height: " + rect2.getHeight());
+            System.out.println("Area: " + rect2.getArea());
+            System.out.println("Perimeter: " + rect2.getPerimeter());
+            System.out.println("\n");
+
+
+        }
+    }
+
